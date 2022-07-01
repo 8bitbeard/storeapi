4157cd22ac223328a35c840cc64920ad6c600d66
Wilton Souza <willsouzafilho@gmail.com>
2022-07-01 11:43:27 -0300

    feat: First commit with Spring initializr structure

---
+++ src/main/java/br/com/pet/storeapi/StoreapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pet.storeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreapiApplication.class, args);
+	}
+
+}
